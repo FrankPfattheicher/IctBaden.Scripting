ec26e1bfd986834766e538d2b681c60d77ce6eef
Frank Pfattheicher <fpf@ict-baden.de>
2020-03-10 16:59:30 +0100

    ScriptCOntext for 48 added

---
+++ IctBaden.Scripting/ScriptContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace IctBaden.Scripting.Library
+{
+    [ComVisible(true)]
+    public class ScriptContext
+    {
+        private readonly Dictionary<string, object> valueList;
+        private QueryValue queryValue;
+
+        public delegate object QueryValue(string key);
+
+        public ScriptContext()
+        {
+            valueList = new Dictionary<string, object>();
+            queryValue = null;
+        }
+
+        public void Clear()
+        {
+            valueList.Clear();
+        }
+
+        public void SetQuery(QueryValue query)
+        {
+            queryValue = query;
+        }
+
+        public object this[string key]
+        {
+            set => valueList[key] = value;
+            get
+            {
+                var val = queryValue?.Invoke(key);
+                if (val != null)
+                    return val;
+                return valueList.ContainsKey(key) ? valueList[key] : null;
+            }
+        }
+
+
+    }
+}
