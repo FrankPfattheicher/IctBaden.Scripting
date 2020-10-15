78b732ee82a521d3c21f8acc906964952f4fdeb9
Frank Pfattheicher <fpf@ict-baden.de>
2020-10-15 21:16:23 +0200

    Warnings removed

--- IctBaden.Scripting/ScriptContext.cs
+++ IctBaden.Scripting/ScriptContext.cs
@@ -1,41 +1,41 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
-namespace IctBaden.Scripting.Library
+namespace IctBaden.Scripting
 {
     [ComVisible(true)]
     public class ScriptContext
     {
-        private readonly Dictionary<string, object> valueList;
-        private QueryValue queryValue;
+        private readonly Dictionary<string, object> _valueList;
+        private QueryValue _queryValue;
 
         public delegate object QueryValue(string key);
 
         public ScriptContext()
         {
-            valueList = new Dictionary<string, object>();
-            queryValue = null;
+            _valueList = new Dictionary<string, object>();
+            _queryValue = null;
         }
 
         public void Clear()
         {
-            valueList.Clear();
+            _valueList.Clear();
         }
 
         public void SetQuery(QueryValue query)
         {
-            queryValue = query;
+            _queryValue = query;
         }
 
         public object this[string key]
         {
-            set => valueList[key] = value;
+            set => _valueList[key] = value;
             get
             {
-                var val = queryValue?.Invoke(key);
+                var val = _queryValue?.Invoke(key);
                 if (val != null)
                     return val;
-                return valueList.ContainsKey(key) ? valueList[key] : null;
+                return _valueList.ContainsKey(key) ? _valueList[key] : null;
             }
         }
 
